f3de3efedf7257277508070b6fe097f8bed5ce1e
clhommews <136691548+clhommews@users.noreply.github.com>
2023-06-26 22:20:15 +0700

    Add automated tests

---
+++ king_garden_sun.lua
@@ -0,0 +1,66 @@
+-- Rise Up in Lua
+
+-- The start of the chorus
+
+-- Lyrics by Lecrae
+
+function riseUp()
+	print("I...I-I-I-I-I'm gon' rise up")
+	print("I'm gon' do what they say I can't")
+	print("I...I-I-I-I-I'm gon' rise up")
+	print("I'm gon' live my life 'til I die")
+end
+
+-- The verses
+
+-- Verse 1
+
+function verse1()
+	print("I was down and out, everybody doubted")
+	print("All of my dreams talked about it, but I never shouted")
+	print("I was backin' down, told to play the sideline")
+	print("Started losin' faith and dyin' on the inside")
+	print("But I'm gonna rise up, I'm gonna try up")
+	print("Gonna spread my wings high up, no more gonna walk on by")
+end
+
+-- Verse 2
+
+function verse2()
+	print("I'm done sittin' back watchin', I'm done hidin' out")
+	print("Cause fear's no longer mine, there's a fire in my soul")
+	print("I'm done hatchin' plans, I'm done runnin' scared")
+	print("I'm done with setting my goals soundin' a little too far")
+	print("No more tryna fit in, I'm done tryna be cool")
+	print("Cause when I stand for something, I'm gonna stand tall, yeah")
+end
+
+-- Verse 3
+
+function verse3()
+	print("I'm gonna face the odds, I'm gonna give it a shot")
+	print("No way I'm backin' down, not gon' live my life off of knots")
+	print("For now on I'm gon' live by design")
+	print("Gonna be a light and aimin' for an higher ground")
+	print("No more livin' scared, no more tryna appease")
+	print("Gonna face the music and rise up to my feet")
+end
+
+
+-- Bridge
+
+function bridge()
+	print("I'm standing up, I'm gonna reach the sky")
+	print("Gonna turn my dark night into day, yeah yeah")
+	print("I'm standing up, I'm gonna touch the sun")
+	print("I'm gonna fill my life up with so much love")
+end
+
+-- Call all the functions
+
+riseUp()
+verse1()
+verse2()
+verse3()
+bridge()
+riseUp()
