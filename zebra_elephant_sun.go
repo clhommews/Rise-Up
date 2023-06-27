f8fb8fdf0906708bac1a225f385b2ab582361320
clhommews <136691548+clhommews@users.noreply.github.com>
2023-06-27 11:26:38 +0700

    Update user permissions

---
+++ zebra_elephant_sun.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Rise Up!")
+
+	// For loop to print out "Rise Up" 2000 times
+	for i := 0; i < 2000; i++ {
+		fmt.Println("Rise Up!")
+	}
+}
