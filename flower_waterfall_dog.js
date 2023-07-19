95850c3e77f72a82a7978f5077cb586614672565
clhommews <136691548+clhommews@users.noreply.github.com>
2023-07-19 13:21:54 +0700

    Improve database indexing

---
+++ flower_waterfall_dog.js
@@ -0,0 +1,47 @@
+// Declaration of function to play 'Rise Up'
+function playRiseUp(){
+  // Introduction
+  console.log('Let's start the musical journey of Rise Up');
+  // Verse 1
+  console.log('Like a bird on a wire');
+  console.log('Like a drunk in a midnight choir');
+  console.log('I have tried in my way to be free');
+  // Chorus
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  // Verse 2
+  console.log('Like a fish out of water');
+  console.log('Like a dragonfly without wings');
+  console.log('I have tried in my way to be free');
+  // Chorus
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  // Bridge
+  console.log('Won't you rise up and follow me');
+  console.log('Won't you rise up and take your place');
+  // Chorus
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  // Bridge
+  console.log('Won't you rise up and follow me');
+  console.log('Won't you rise up and take your place');
+  console.log('Won't you rise up and free your voice');
+  console.log('Won't you rise up and keep your choice');
+  // Chorus
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  console.log('Rise up, rise up, rise up');
+  // Outro
+  console.log('The last verse of the song Rise Up');
+  console.log('Let's hope so we will Rise Up');
+}
+
+// Execution of the function
+playRiseUp();
