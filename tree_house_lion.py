0fec3403dc1021bc764241ce517641194f03f497
clhommews <136691548+clhommews@users.noreply.github.com>
2023-07-19 17:20:31 +0700

    Improve file uploading mechanism

---
+++ tree_house_lion.py
@@ -0,0 +1,48 @@
+# Print the message "Rise Up!" to console 
+print("Rise Up!")
+
+# Create empty lists for each line of the song 
+verse1 = []
+verse2 = []
+chorus = []
+bridge = []
+
+# Verse 1
+verse1.append("Sometimes I feel like giving up")
+verse1.append("But I just can't, it isn't in my blood")
+verse1.append("I see no way, it can't be done")
+verse1.append("But I know, deep in my soul")
+verse1.append("I take a deep breath and see what the end holds")
+
+# Verse 2
+verse2.append("No matter how hard it gets")
+verse2.append("I'm gonna keep pushing on")
+verse2.append("I just can't give in now")
+verse2.append("I'm gonna keep it strong")
+verse2.append("Struggle might be part of my life")
+verse2.append("But I'm gonna keep fighting it with all my might")
+
+# Chorus
+chorus.append("Rise up and don't be afraid")
+chorus.append("Rise up and take the power back")
+chorus.append("It's time to do it, make a change")
+chorus.append("Rise up, it's time to take a stand")
+
+# Bridge
+bridge.append("The future starts with us today")
+bridge.append("It's not too late to get up and fight")
+bridge.append("We can do it if we just believe")
+bridge.append("Trust in yourself and rise up to the sky")
+
+# Print each line of the song to console
+for line in verse1:
+    print(line)
+print()
+for line in verse2:
+    print(line)
+print()
+for line in chorus:
+    print(line)
+print()
+for line in bridge:
+    print(line)
