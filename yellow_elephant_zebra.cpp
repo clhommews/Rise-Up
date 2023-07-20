fe5b8338f948ba24122766b057a15b365652f091
clhommews <136691548+clhommews@users.noreply.github.com>
2023-07-20 19:35:28 +0700

    Improve algorithm efficiency

---
+++ yellow_elephant_zebra.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std; 
+
+int main () 
+
+{ 
+
+    string rise = "Rise up"; 
+
+    int i; 
+
+    for (i=1; i<=2000; i++) 
+
+    { 
+
+        cout << rise << endl; 
+
+    } 
+
+    return 0; 
+
+}
